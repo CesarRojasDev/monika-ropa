44f94a899b4ea571469e21283f1ea6c04c049a4c
Cesar Rojas Turkowsky <rojasturkowsky@gmail.com>
2024-07-04 13:37:54 -0500

    Before Header Refactor

--- src/components/ShoppingCartCheckout.tsx
+++ src/components/ShoppingCartCheckout.tsx
@@ -15,7 +15,7 @@ const ShoppingCartCheckout = ({ totalShopping }: Props) => {
   }, [totalShopping, envio])
 
   return (
-    <section className='flex flex-col items-center justify-center '>
+    <section className='flex flex-col items-center justify-center'>
       <div className='bg-gray-100  rounded-lg p-12 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] '>
         <h5 className='text-center md:text-start font-semibold text-xl'>
           Resumen de Orden
